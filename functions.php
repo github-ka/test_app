48b5415e74a4353b649a083802ca466aea5a0fc2
兼城賢人 <>
2019-08-29 19:51:58 +0900

    重複チェックfunctionを作成

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
-//データの受け取り・受け渡しとDBへの処理を依頼する機能をまとめるファイルを作成します
-
+//データの受け取り・受け渡しとDBへの処理を依頼する機能をまとめるファイルを作成
 
 require_once('connection.php');//外部ファイル読み込み
+ini_set('session.save_path', './session_file'); //セッションファイルの保存先指定
+// session_save_path('./sessionfile');　セッションファイルの保存先指定　上と同じ
 session_start();
-
 
 // エスケープ処理
 function h($text)
@@ -20,13 +20,19 @@ function setToken()
     //sha1で ハッシュ化・uniqid関数＝マイクロ秒単位の現在時刻に基づく13文字の文字列が作成される。引数を指定すると頭に引数が設置される・第一引数で乱数表示をし第二引数にtrueを入れることで『.』+13の文字列を追加できる・(初期値はfalse)
 }
 
+//一つ前のURLに戻る
+function oneReturn()
+{
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    exit();
+}
+
 // SESSIONに格納されてたtokenのチェックを行いCSRF対策を行う
 function checkToken($token)
 {
     if (empty($_SESSION['token']) || ($_SESSION['token'] !== $token)) {
         $_SESSION['err'] = '不正な操作です';
-        header('Location: '.$_SERVER['HTTP_REFERER']);
-        exit();
+        oneReturn();
     }
     return true;
 }
@@ -40,28 +46,17 @@ function unsetSession()
 
 function saveDataAfterRedirect()
 {
-  // echo '<pre>';
-  //     var_dump($_SESSION['token']);
-  // echo '</pre>';
-  // echo '<pre>';
-  //     var_dump($_POST);
-  // echo '</pre>';
-  // exit;
-
-    // unsetSession();
-  // var_dump($_POST);
-  // var_dump($_SESSION['token']);
     $post = $_POST;
     $path = checkReferer();
     if(validate($post)){
-      if ($path === '/new.php') {
-          createData($post);
-      } elseif($path === '/edit.php') {
-          updateData($post);
-      } else {
-          deleteData($post['id']);
-      }
-      return 'index';
+        if ($path === '/new.php') {
+            createData($post);
+        } elseif($path === '/edit.php') {
+            updateData($post);
+        } else {
+            deleteData($post['id']);
+        }
+        return 'index';
     } else {
         return 'error';
     }
@@ -83,17 +78,27 @@ function checkReferer()
     return $httpArr['path'];//urlの中のpathを取得してreturnで返している
 }
 
+//
+function checkdiff($post)
+{
+    echo'checkdiffです';
+    echo'<br>';
+    // return true or false の仕組み
+    if(diffData($post) == '0'){
+        return true;
+    } else {
+        $_SESSION['err'] = 'すでに同じ内容が存在します';
+        oneReturn();
+    }
+}
 
 
 function createData($post) //データの受け取り処理
 {
-  if (checkToken($post['token'])) {
-    createTodoData($post['todo']);
-  }
-
-  // createTodoData($post['todo']);  // 実際にPOSTされた際に上記の処理にデータを渡す
+	if(checkToken($post['token']) && checkdiff($post['todo'])) {
+        createTodoData($post['todo']);
+	}
 }
-
 
 
 //index.php(元はindex.html)で呼び出してTODOリスト一覧を表示させる
@@ -106,10 +111,9 @@ function todoList()
 // 下記で記述したupdataData()にPOSTデータを渡して呼び出せばDBのupdata処理が実行させる
 function updateData($post)
 {
-  if (checkToken($post['token'])) {
-    updateTodoData($post['id'], $post['todo']);
-  }
-  // updateTodoData($post['id'],$post['todo']);
+    if (checkToken($post['token'])  && checkdiff($post['todo'])) {
+        updateTodoData($post['id'], $post['todo']);
+    }
 }
 
 
